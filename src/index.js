971cce470517e20f5816725c2205387a682f50af
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-05-17 22:09:51 -0300

    adicioana exemplo de PDF

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-const calcularInss = require("./calculo_inss");
+const readline = require('readline')
+const PdfDocument  = require('pdfkit')
+const fs = require('fs')
 
-console.log(calcularInss(5600))
+const input = readline.createInterface(
+    process.stdin,
+    process.stdout
+);
+
+input.question("Nome do funcionário ?", (nomeDigitado) => {
+    const doc = new PdfDocument()
+    doc.pipe(fs.createWriteStream(`${nomeDigitado}.pdf`))
+
+    doc.fontSize(16)
+
+    doc.text("Folha de pagamento")
+    doc.text(`Funcionario: ${nomeDigitado}`)
+    doc.end()
+})
+
+
